d147410231f71af03bdc9f3161d1d9f408b5e851
bhargav3vedee <30555612+bhargav3vedee@users.noreply.github.com>
2017-10-26 14:25:51 +0530

    Add files via upload
    this folder contains js, css. copy both these file and folder to your root folder where you want to use file manager.

---
+++ file_browser/connectors/jqueryFileTree.php
@@ -0,0 +1,29 @@
+<?php
+
+$_POST['dir'] = urldecode($_POST['dir']);
+
+if( file_exists($_POST['dir']) ) {
+    
+	$files = scandir($_POST['dir']);
+        
+	natcasesort($files);
+	if( count($files) > 2 ) { /* The 2 accounts for . and .. */
+		echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
+		// All dirs
+		foreach( $files as $file ) {
+			if( file_exists( $_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($_POST['dir'] . $file) ) {
+				echo "<li class=\"directory collapsed\"><a href=\"#\"  onclick=\"dirVal('".$_POST['dir'] . $file."')\" rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
+			}
+		}
+		// All files
+		foreach( $files as $file ) {
+			if( file_exists( $_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir( $_POST['dir'] . $file) ) {
+				$ext = preg_replace('/^.*\./', '', $file);
+				echo "<li class=\"file ext_$ext\"><a onclick=\"dirVal('".$_POST['dir'] . $file."')\" href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
+			}
+		}
+		echo "</ul>";	
+	}
+}
+
+?>
